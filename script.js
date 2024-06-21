57164ecef0c19bed207d4341e7a533208cb81640
mad-proton <pritamsahatfl1998@gmail.com>
2024-06-21 23:53:38 +0600

    Updated mobile menu

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuToggle = document.getElementById('menu-toggle');
+//     const menu = document.querySelector('.menu');
+//     menuToggle.addEventListener('change', function() {
+//       if (menuToggle.checked) {
+//         menu.style.display = 'flex';
+//       } else {
+//         menu.style.display = 'none';
+//       }
+//     });
+//   });
